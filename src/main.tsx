7e8ea33b36607c908967fd20359aa004693063b0
xlliu <liu.xuelong@163.com>
2024-04-28 11:35:35 +0800

    init

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { THEME, TonConnectUIProvider } from '@tonconnect/ui-react';
 import "./react-i18next/i18n";
 
 // const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
-const manifestUrl = 'https://xlliu.github.com/Kakegurui/tonconnect-manifest.json'
+const manifestUrl = 'https://jeerclub.xyz/tonconnect-manifest.json'
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	// <TonConnectUIProvider manifestUrl={manifestUrl}>
 	<TonConnectUIProvider
