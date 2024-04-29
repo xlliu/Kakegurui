ac1f3035a1c5f75ac66eec2e8cd9e39ec289d076
Spark <Spark@wf.local>
2024-04-29 21:42:17 +0800

    amount

--- src/main.tsx
+++ src/main.tsx
@@ -36,7 +36,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 			]
 		}}
 		actionsConfiguration={{
-			twaReturnUrl: 'https://t.me/DemoDappWithTonConnectBot/demo'
+			twaReturnUrl: 'https://t.me/KKGurui_bot'
 		}}
 	>
 		<NextUIProvider>
