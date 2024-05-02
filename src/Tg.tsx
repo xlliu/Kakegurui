f22f8b57b0b5f179578661a4056c929407fb615b
Spark <Spark@wf.local>
2024-05-02 20:24:05 +0800

    init

--- src/Tg.tsx
+++ src/Tg.tsx
@@ -10,7 +10,6 @@ export default function Tg(props) {
   const { t } = useTranslation();
   useEffect(() => {
     async function getValue() {
-      // const sc = document.getElementById('telegram-discussion-jeerclub-3-1');
       const view = document.getElementById('content-view-tg');
       
       const myScript = document.createElement("script");
@@ -24,33 +23,13 @@ export default function Tg(props) {
       myScript.setAttribute('data-dark',"1" );
       view!!.appendChild(myScript);
     }
-    // setTimeout(getValue, 3000); // 每5秒自动刷新数据
-    // const intervalId = setInterval(getValue, 5000); // 每5秒自动刷新数据
-    // return () => {
-    //   // view!!.removeChild(sc!!);
-    //   clearInterval(intervalId); // 在组件卸载时清除定时器
-    // }
-   
     if (window.matchMedia('(min-width: 768px)').matches) {
       getValue()
-      // console.log('按钮被点击了');
     }
   }, []);
 
   return (
     <Card className="min-w-[200px] min-h-[500px] hidden md:block" radius="sm">
-      <div id="view-tg">
-        {/* <script 
-          type="text/javascript"
-          // async 
-          src="https://telegram.org/js/telegram-widget.js?22" 
-          data-telegram-discussion="jeerclub/3" 
-          data-comments-limit="5" 
-          data-height="500"  
-          data-colorful="1" 
-          data-dark="1" ></script> */}
-      </div>
-      
       <div id="content-view-tg">
 
       </div>
